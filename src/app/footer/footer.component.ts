c4eb0bfc4aa424ad51d263be2644e3b880ef459e
Shivam Pandey <shivam.pandey@squarepixelstudios.net>
2022-05-07 17:16:46 +0530

    completed footer with responsive

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  faGlobe = faGlobe;
+  faPhoneVolume = faPhoneVolume;
+  faLocationDot = faLocationDot;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
